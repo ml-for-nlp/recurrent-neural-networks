90ff4ef9c7615fb1af66bfa68a222013869a4118
minimalparts <aurelie.herbelot@cantab.net>
2022-04-05 08:03:41 +0200

    Fixed docopt

--- cat-char-rnn.py
+++ cat-char-rnn.py
@@ -4,10 +4,10 @@ Modified by Aurelie Herbelot.
 BSD License
 
 Usage:
-min-char-rnn.py <input_file> --seq_length=NUM --hidden_size=NUM --num_epochs=NUM --lr=<lr>
+    min-char-rnn.py <input_file> --seq_length=NUM --hidden_size=NUM --num_epochs=NUM --lr=<lr>
 
 Options:
--h --help     Show this screen.
+    -h --help     Show this screen.
 """
 
 
@@ -16,29 +16,6 @@ import numpy as np
 from docopt import docopt
 import sys
 
-# data I/O
-args = docopt(__doc__)
-print(args)
-input_file = args["<input_file>"]
-data = open(input_file, 'r').read() # should be simple plain text file
-chars = list(set(data))
-data_size, vocab_size = len(data), len(chars)
-print('data has %d characters, %d unique.' % (data_size, vocab_size))
-char_to_ix = { ch:i for i,ch in enumerate(chars) }
-ix_to_char = { i:ch for i,ch in enumerate(chars) }
-
-# hyperparameters
-hidden_size = int(args["--hidden_size"])    # size of hidden layer of neurons
-seq_length = int(args["--seq_length"])      # number of steps to unroll the RNN for
-num_epochs = int(args["--num_epochs"])      # number of steps to unroll the RNN for
-learning_rate = float(args["--lr"])
-
-# model parameters
-Wxh = np.random.randn(hidden_size, vocab_size)*0.01 # input to hidden
-Whh = np.random.randn(hidden_size, hidden_size)*0.01 # hidden to hidden
-Why = np.random.randn(vocab_size, hidden_size)*0.01 # hidden to output
-bh = np.zeros((hidden_size, 1)) # hidden bias
-by = np.zeros((vocab_size, 1)) # output bias
 
 def lossFun(inputs, targets, hprev):
   """
@@ -88,51 +65,76 @@ def sample(h, seed_ix, n):
     h = np.tanh(np.dot(Wxh, x) + np.dot(Whh, h) + bh)
     y = np.dot(Why, h) + by
     p = np.exp(y) / np.sum(np.exp(y))
-    ix = np.random.choice(range(vocab_size), p=p.ravel())  #sampling from distribution with higher probability to pick chars with high ps
-    #ix = p.ravel().argmax()
+    #ix = np.random.choice(range(vocab_size), p=p.ravel())  #sampling from distribution with higher probability to pick chars with high ps
+    ix = p.ravel().argmax()
     x = np.zeros((vocab_size, 1))
     x[ix] = 1
     ixes.append(ix)
   return ixes
 
 
-n, p = 0, 0
-mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-mbh, mby = np.zeros_like(bh), np.zeros_like(by) # memory variables for Adagrad
-smooth_loss = -np.log(1.0/vocab_size)*seq_length # loss at iteration 0
 
-epoch_count = 0
-while epoch_count < num_epochs:
-  # prepare inputs (we're sweeping from left to right in steps seq_length long)
-  if p+seq_length+1 >= len(data) or n == 0: 
-    hprev = np.zeros((hidden_size,1)) # reset RNN memory
-    p = 0 # go from start of data
-    # sample from the model
-    if epoch_count > 0 and epoch_count % 10 == 0:
-        print("EPOCH",epoch_count)
-        sample_ix = sample(hprev, inputs[0], 100)
-        txt = ''.join(ix_to_char[ix] for ix in sample_ix)
-        print('----\n %s \n----' % (txt, ))
-        print('iter %d, loss: %f' % (n, smooth_loss)) # print progress
-        print('----\n')
-    epoch_count+=1
-  inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
-  targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
-  #print("X:",''.join([ix_to_char[i] for i in inputs]))
-  #print("T:",''.join([ix_to_char[i] for i in targets]))
+if __name__ == '__main__':
+    args = docopt(__doc__, version='The amazing cat RNN tutorial 0.1')
+    print(args)
+    input_file = args["<input_file>"]
+    data = open(input_file, 'r').read() # should be simple plain text file
+    chars = list(set(data))
+    data_size, vocab_size = len(data), len(chars)
+    print('data has %d characters, %d unique.' % (data_size, vocab_size))
+    char_to_ix = { ch:i for i,ch in enumerate(chars) }
+    ix_to_char = { i:ch for i,ch in enumerate(chars) }
+
+    # hyperparameters
+    hidden_size = int(args["--hidden_size"])    # size of hidden layer of neurons
+    seq_length = int(args["--seq_length"])      # number of steps to unroll the RNN for
+    num_epochs = int(args["--num_epochs"])      # number of steps to unroll the RNN for
+    learning_rate = float(args["--lr"])
+
+    # model parameters
+    Wxh = np.random.randn(hidden_size, vocab_size)*0.01 # input to hidden
+    Whh = np.random.randn(hidden_size, hidden_size)*0.01 # hidden to hidden
+    Why = np.random.randn(vocab_size, hidden_size)*0.01 # hidden to output
+    bh = np.zeros((hidden_size, 1)) # hidden bias
+    by = np.zeros((vocab_size, 1)) # output bias
+
+    n, p = 0, 0
+    mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
+    mbh, mby = np.zeros_like(bh), np.zeros_like(by) # memory variables for Adagrad
+    smooth_loss = -np.log(1.0/vocab_size)*seq_length # loss at iteration 0
+
+    epoch_count = 0
+    while epoch_count < num_epochs:
+      # prepare inputs (we're sweeping from left to right in steps seq_length long)
+      if p+seq_length+1 >= len(data) or n == 0: 
+        hprev = np.zeros((hidden_size,1)) # reset RNN memory
+        p = 0 # go from start of data
+        # sample from the model
+        if epoch_count > 0 and epoch_count % 10 == 0:
+            print("EPOCH",epoch_count)
+            sample_ix = sample(hprev, inputs[0], 100)
+            txt = ''.join(ix_to_char[ix] for ix in sample_ix)
+            print('----\n %s \n----' % (txt, ))
+            print('iter %d, loss: %f' % (n, smooth_loss)) # print progress
+            print('----\n')
+        epoch_count+=1
+      inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
+      targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
+      #print("X:",''.join([ix_to_char[i] for i in inputs]))
+      #print("T:",''.join([ix_to_char[i] for i in targets]))
 
 
-  # forward seq_length characters through the net and fetch gradient
-  #print(hprev)
-  loss, dWxh, dWhh, dWhy, dbh, dby, hprev = lossFun(inputs, targets, hprev)
-  smooth_loss = smooth_loss * 0.999 + loss * 0.001
-  
-  # perform parameter update with Adagrad
-  for param, dparam, mem in zip([Wxh, Whh, Why, bh, by], 
-                                [dWxh, dWhh, dWhy, dbh, dby], 
-                                [mWxh, mWhh, mWhy, mbh, mby]):
-    mem += dparam * dparam
-    param += -learning_rate * dparam / np.sqrt(mem + 1e-8) # adagrad update
+      # forward seq_length characters through the net and fetch gradient
+      #print(hprev)
+      loss, dWxh, dWhh, dWhy, dbh, dby, hprev = lossFun(inputs, targets, hprev)
+      smooth_loss = smooth_loss * 0.999 + loss * 0.001
+      
+      # perform parameter update with Adagrad
+      for param, dparam, mem in zip([Wxh, Whh, Why, bh, by], 
+                                    [dWxh, dWhh, dWhy, dbh, dby], 
+                                    [mWxh, mWhh, mWhy, mbh, mby]):
+        mem += dparam * dparam
+        param += -learning_rate * dparam / np.sqrt(mem + 1e-8) # adagrad update
 
-  p += seq_length # move data pointer
-  n += 1 # iteration counter 
+      p += seq_length # move data pointer
+      n += 1 # iteration counter 
